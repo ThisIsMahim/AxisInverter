7fdb7416cd6e8a3d96daeb9eed9e7ccb001b270a
wzium <michalwojtowicz_2001@tlen.pl>
2020-11-23 15:41:22 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# AxisInverter
+A program which inverts the x and y mouse axis.\
+Open this app and press the key to set it as your switch.\
+Once you have it set, just press it to invert your mouse movement!\
+You can return the movement of your mouse back to normal by pressing the key again.
+
+![Console](https://i.imgur.com/DEiynGo.png)
+
+"w", "a", "s" and "d" and disabled by default, but you can change it in the code.
+Also, special keys like "Shift" are not supported.
---
+++ main.py
@@ -0,0 +1,111 @@
+import pyautogui
+from time import sleep
+from pynput import keyboard
+from pynput import mouse
+from threading import Thread
+import os
+
+
+class KeyboardListener:
+    def __init__(self):
+        os.system("title AxisInverter")
+        self.banned_keys = ["w", "a", "s", "d"]  # List of keys that cannot be the switch.
+        # I'm blocking "w", "a", "s", "d" just because they are used in games for movement.
+        # Feel free to change it.
+        self.parsed_keys = []
+        for keys in self.banned_keys:
+            self.parsed_keys.append(self.key_parser(keys))
+        self.mouse = mouse.Controller()
+        self.switch_set = False
+        self.inverted = False
+        self.thread = None
+        self.first_usage = True
+        self.switch = None
+        self.mouse = mouse.Controller()
+        self.minx, self.miny = (0, 0)
+        first_pos = pyautogui.position()
+        self.mouse.position = (99999, 99999)  # Getting the max x and max y of your screen.
+        self.maxx, self.maxy = pyautogui.position()
+        self.mouse.position = first_pos  # Setting your previous cursor position.
+        print("Press the key to choose a switch...")
+
+        with keyboard.Listener(on_press=self.on_press, on_release=self.on_release) as keyboard_listener:
+            keyboard_listener.join()
+
+    @staticmethod
+    def key_parser(key: str):
+        parsed_key = keyboard.KeyCode.from_char(key)
+        return parsed_key
+
+    @staticmethod
+    def equals(n1: int, n2: int) -> bool:
+        if abs(n1 - n2) < 3:
+            return True
+        else:
+            return False
+
+    def on_press(self, key):
+        if not self.switch_set:
+            try:
+                if key in self.parsed_keys:
+                    print("{0} cannot be a switch".format(key.char.upper()))
+                    print("Choose another key...")
+                else:
+                    print("{0} is your switch button.".format(key.char.upper()))
+                    self.switch_set = True
+                    self.switch = key
+                    self.first_usage = True
+            except AttributeError:  # Special keys like shift aren't supported.
+                print("Choose another key, this one is not supported.")
+
+    def on_release(self, key):
+        if self.switch_set:
+            if key == self.switch:
+                if self.first_usage:  # It won't invert our mouse automatically after we choose the switch.
+                    self.inverted = False
+                    self.first_usage = False
+                    print("Press {0} to invert your mouse movement".format(key.char.upper()))
+                else:
+                    if not self.inverted:
+                        self.inverted = True
+                        print("Your mouse movement is now inverted.")
+                        # Threading is used to manage the keyboard listener AND to do the inverting loop.
+                        # If we wouldn't use threading, we wouldn't be able to switch the inversion with a key.
+                        self.thread = Thread(target=self.inversion)
+                        self.thread.start()
+                    else:
+                        self.inverted = False
+                        # If we press the switch button again, mouse movement will be back to normal.
+                        print("Your mouse movement is back to normal.")
+                        if self.thread.is_alive():
+                            self.thread.join()
+
+    def inversion(self):
+        while self.inverted:
+            x1, y1 = pyautogui.position()
+            # Increase this value if you want your computer to use less resources.
+            # If you want the movement to be smooth, keep this value low, for example 0.001
+            sleep(0.001)
+            x2, y2 = pyautogui.position()
+            if self.equals(x1, self.maxx):
+                x1 = x1 - 2
+            if self.equals(x2, self.maxx):
+                x2 = x2 - 2
+            if self.equals(y1, self.maxy):
+                y1 = y1 - 2
+            if self.equals(y2, self.maxy):
+                y2 = y2 - 2
+            if self.equals(x1, self.minx):
+                x1 = x1 + 2
+            if self.equals(x2, self.minx):
+                x2 = x2 + 2
+            if self.equals(y1, self.miny):
+                y1 = y1 + 2
+            if self.equals(y2, self.miny):
+                y2 = y2 + 2
+            dx = x2 - x1
+            dy = y2 - y1
+            self.mouse.position = (x1 - dx, y1 - dy)
+
+
+main = KeyboardListener()
